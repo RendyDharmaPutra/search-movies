71c5d96346d75c18e14a59ef14e5f8bc36f700b9
Rendy DharmaPutra <72882576+RendyDharmaPutra@users.noreply.github.com>
2023-03-28 17:46:55 +0700

    Update movies.js

--- src/app/movies.js
+++ src/app/movies.js
@@ -5,7 +5,7 @@ import useSWR from 'swr';
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Movies(props) {
-  const { data, error } = useSWR(`http://www.omdbapi.com/?apikey=37642a8&s=${props.query}`, fetcher);
+  const { data, error } = useSWR(`https://www.omdbapi.com/?apikey=37642a8&s=${props.query}`, fetcher);
   return (
     <div className="p-3 my-5 flex flex-wrap justify-around ">
       {data &&
